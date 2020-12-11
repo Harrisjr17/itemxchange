3b571d5292ca906116e06608147c3f5c81641303
harrisjr17 <harrisjrkumchulesi@gmail.com>
2020-12-11 09:32:46 +0200

    setting up the products & displaying on the home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-        
-<div class="container my-5">
-    <h1 class="display-5">Laravel 8 with Bootstrap 4 and Auth!</h1>
-</div>
-        @endsection
+    <div class="container my-3">
+        <h2 class="display-5">Products</h2>
+
+        <div class="row">
+           @foreach ($products as $product)
+            <div class="col-4 mb-2">
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('it.jpg') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $product->name }}</h4>
+                        <p class="card-text">{{ $product->description }}</p>
+                    </div>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Add to cart</a>
+                    </div>
+                </div>
+            </div>
+           @endforeach
+
+
+        </div>
+    </div>
+@endsection
